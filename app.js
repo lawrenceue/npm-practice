baede38eaa83549cef8a9c5fa50757f157046ec5
larryu <larryu@larryu-HP-Pavilion-dm4-Notebook-PC.(none)>
2015-11-08 20:51:35 -0500

    second

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log('starting password manager');
+
+var storage = require('node-persist');
+storage.initSync();
+
+
+// account.name Facebook
+// account.username user12@!
+// account.password password123
+
+function createAccount(account){
+    var accounts = storage.getItemSync('accounts');
+    // if accounts is undefined, use typeOf
+    //   set accounts = to an empty array
+    
+    // call push to push on new account
+  
+    // return account
+}
+
+function getAccount(accountName){
+    // fetch the account using getItemSync()
+
+    // var accounts use getItemSync
+    // use forEach to iterate over array and return matching account
+    // else undefined
+}
